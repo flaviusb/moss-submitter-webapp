8694f3a800795769f07939c47091c5835ab700e0
Justin Marsh (:flaviusb) <justin.marsh@flaviusb.net>
2017-06-26 15:22:11 +1200

    Typechecking, dependencies, typo fix, lots of character encoding fixes.

--- noodling.hs
+++ noodling.hs
@@ -15,15 +15,20 @@ import Data.Text.Encoding (decodeUtf8, encodeUtf8)
 import qualified Data.ByteString as BS
 import System.IO
 
+len = BS.length
 
 data FileData = FileData { contents :: Text, id :: Text, lang :: Text, path :: Text, size :: Text }
 
+data Switch = X
+
+
 popFileData :: Text -> IO FileData
 popFileData path = do
-  handle <- openFile path ReadMode  
+  handle <- openFile (T.unpack path) ReadMode  
   contents <- hGetContents handle  
   hClose handle
-  return FileData { contents=contents, id=..., lang=..., path=path, size=(length (encodeUtf8 contents)) }
+  let output = T.pack contents
+  return FileData { contents=output, id="", lang="", path=path, size=(T.pack (show $ len $ encodeUtf8 output)) }
 
 uploadFile :: FileData -> Socket -> IO ()
 uploadFile FileData{..} sock = do
@@ -31,7 +36,7 @@ uploadFile FileData{..} sock = do
   sendAll sock (encodeUtf8 opening_stanza)
 
 submitToMoss :: [Switch] -> [FileData] -> IO ()
-submitToMoss options files = withSocketDo $ do
+submitToMoss options files = withSocketsDo $ do
   let hints = defaultHints { addrFlags = [ AI_ALL, AI_NUMERICSERV ] }
   addr:_ <- getAddrInfo (Just hints) (Just "moss.stanford.edu") (Just "6790")
   sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
