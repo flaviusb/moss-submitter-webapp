eb46640cd284a889613d02fa47e97cb8a3f6f611
Justin Marsh (:flaviusb) <justin.marsh@flaviusb.net>
2017-06-30 14:52:04 +1200

    Move noodling into Home.
    We now have the functions in the main page handler for talking to Moss.

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -9,7 +9,7 @@
 
 module Handler.Home where
 
-import Import
+import Import hiding (decodeUtf8, encodeUtf8, hGetContents)
 import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
 import Text.Julius (RawJS (..))
 import Network.Socket hiding (recv)
@@ -179,3 +179,53 @@ switchesField = Field {
 
 commentIds :: (Text, Text, Text)
 commentIds = ("js-commentForm", "js-createCommentTextarea", "js-commentList")
+
+userID = "12345"
+
+sendPrologue :: Switch -> Socket -> IO Text
+sendPrologue Switch{..} sock = do
+  let prologue = (encodeUtf8 $ T.concat [
+          "moss ", userID, "\n",
+          "directory ", if filesByDirectory then "1" else "0", "\n",
+          "X ", if experimental then "1" else "0", "\n",
+          "maxmatches ", T.pack $ show matchThreshold, "\n",
+          "show ", T.pack $ show numberOfMatchesToShow, "\n",
+          "language ", language, "\n"
+        ])
+  sendAll sock prologue
+  -- now check whether the options are all supported
+  -- At this point, that theoretically means just the language
+  supported <- recv sock 16
+  return $ decodeUtf8 supported
+
+popFileData :: Text -> IO FileData
+popFileData path = do
+  handle <- openFile (T.unpack path) ReadMode  
+  contents <- hGetContents handle  
+  hClose handle
+  let output = T.pack contents
+  return FileData { contents=output, id="", lang="", path=path, size=(T.pack (show $ len $ encodeUtf8 output)) }
+
+uploadFile :: Socket -> FileData -> IO ()
+uploadFile sock FileData{..} = do
+  let opening_stanza = T.concat ["file ", id, " ", lang, " ", size, " ", path, "\n" ]
+  sendAll sock (encodeUtf8 opening_stanza)
+
+submitToMoss :: Switch -> [FileData] -> IO Text
+submitToMoss options files = withSocketsDo $ do
+  let hints = defaultHints { addrFlags = [ AI_ALL, AI_NUMERICSERV ] }
+  addr:_ <- getAddrInfo (Just hints) (Just "moss.stanford.edu") (Just "6790")
+  sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
+  bind sock (addrAddress addr)
+  -- Now we do blah blah blah
+  supported <- sendPrologue options sock
+  if (T.dropWhileEnd (=='\n') supported) == "no"
+    then sendAll sock (encodeUtf8 "end\n") -- Something in the prologue wasn't supported - bail out here
+  else
+    -- We continue, and upload all the files
+    mapM_ (uploadFile sock) files
+  sendAll sock $ encodeUtf8 $ T.concat ["query 0 ", comment options, "\n"]
+  response <- recv sock 1024
+  sendAll sock $ encodeUtf8 "end\n"
+  close sock
+  return $ decodeUtf8 response
