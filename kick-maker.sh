6439d75cfa70a5baabd95e43b01ba57d6eccfaed
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2021-02-02 15:57:30 +1300

    Add kick maker.

---
+++ kick-maker.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+usage() { cat <<HELP
+kick-maker.sh: Generate a kick.sh with MOSSUserId in it
+Usage:  kick-maker.sh [MossUserId]
+Options:
+  -h/--help  This help text
+HELP
+exit 0;
+}
+
+if [ $# = 0 ]; then
+  usage
+fi
+
+MOSSUserID="000000000"
+if [ $# = 1 ]; then
+  case "$1" in
+    -h)
+      usage
+      ;;
+    --help)
+      usage
+      ;;
+    *)
+      MOSSUserID="$1"
+      ;;
+  esac
+else
+  usage
+fi
+
+
+cat > kick.sh <<END
+#!/bin/bash
+
+foo=\`screen -list | grep [.]mosssubmitterwebapp\`
+if [ -z "\$foo" ]
+then
+  MOSSUserID=$MOSSUserID
+  screen -d -m -S mosssubmitterwebapp stack exec -- yesod devel
+fi
+END
+chmod u+x kick.sh
